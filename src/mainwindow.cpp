ca0208a9a5c448809723be39e2a99b2634711891
Luís Pereira <luis.artur.pereira@gmail.com>
2010-06-11 14:20:43 -0700

    Removes trailing space

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -223,7 +223,7 @@ void MainWindow::toggleWarningWidget(bool on)
 
 bool MainWindow::loadFile(const QString &fileName)
 {
-    QFile file(fileName); 
+    QFile file(fileName);
     if (!file.open(QIODevice::ReadOnly | QIODevice::Text)) {
         QMessageBox::warning(this, tr("Error opening file"),
                              tr("Cannot read file %1:\n%2.")
