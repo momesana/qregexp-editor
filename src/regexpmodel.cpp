7c30035bb43b5712d340d51091403de7c46e2443
Luís Pereira <luis.artur.pereira@gmail.com>
2011-01-21 00:36:19 +0000

    Updates the copyright disclaimer project name
    QRegExp-Editor is the project 'official' name. Use it consistently.
    
    Signed-off-by: Luís Pereira <luis.artur.pereira@gmail.com>

--- src/regexpmodel.cpp
+++ src/regexpmodel.cpp
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2010 M. Mehdi Salem Naraghi <momesana@yahoo.de>
  *
- * This file is part of Regexp-Editor.
+ * This file is part of QRegexp-Editor.
  *
- * Regexp-Editor is free software: you can redistribute it and/or modify
+ * QRegexp-Editor is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Foobar is distributed in the hope that it will be useful,
+ * QRegExp-Editor is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+ * along with QRegExp-Editor.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "regexpmodel.h"
